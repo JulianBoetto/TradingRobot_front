0d2b23cd5eac9ec0fb374adc6155578d3f4c4ed7
Julián Boetto <julib_8724@hotmail.com>
2022-09-03 15:56:39 +0200

    Clean layout

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,4 +1,4 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { LaptopOutlined } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu } from 'antd';
 import React from 'react';
 import Orders from '../pages/orders/orders';
@@ -7,21 +7,18 @@ import colours from "../lib/colours";
 
 
 const { Header, Content, Sider } = Layout;
-const items1 = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+
+const items2 = [LaptopOutlined].map((icon, index) => {
   const key = String(index + 1);
   return {
     key: `sub${key}`,
     icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
+    label: `Home`,
+    children: new Array(1).fill(null).map((_, j) => {
       const subKey = index * 4 + j + 1;
       return {
         key: subKey,
-        label: `option${subKey}`,
+        label: `Open orders`,
       };
     }),
   };
