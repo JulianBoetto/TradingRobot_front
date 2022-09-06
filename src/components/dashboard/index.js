5e415810f5fafb9abcf20767be46b0b73d9424ff
Julián Boetto <julib_8724@hotmail.com>
2022-09-06 23:32:59 +0200

    Order view OK

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -61,12 +61,12 @@ const Dashboard = () => {
                     <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
                         <div className="position-sticky pt-3 sidebar-sticky">
                             <ul className="nav flex-column">
-                                <li className="nav-item">
+                                {/* <li className="nav-item">
                                     <a className="nav-link active" aria-current="page">
                                         <span data-feather="home" className="align-text-bottom"></span>
                                         Dashboard
                                     </a>
-                                </li>
+                                </li> */}
                                 <li className="nav-item">
                                     <a className="nav-link">
                                         <span data-feather="file" className="align-text-bottom"></span>
@@ -89,9 +89,9 @@ const Dashboard = () => {
                     </nav>
 
                     <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                        {/* <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                             <h1 className="h2">Dashboard</h1>
-                            {/* <div className="btn-toolbar mb-2 mb-md-0">
+                            <div className="btn-toolbar mb-2 mb-md-0">
                                 <div className="btn-group me-2">
                                     <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
                                     <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
@@ -100,15 +100,15 @@ const Dashboard = () => {
                                     <span data-feather="calendar" className="align-text-bottom"></span>
                                     This week
                                 </button>
-                            </div> */}
-                        </div>
-                        {
+                            </div>
+                        </div> */}
+                        {/* {
                             loading ? (
                                 <Spin />
                             ) : (
                                 <ApexChart />
                             )
-                        }
+                        } */}
 
                         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
                             <h1 className="h2">Orders</h1>
